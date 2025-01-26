9b075f250db9e6652d60072ee8dd8cf4964048e5
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-01-26 13:16:01 +0200

    latest changes

--- verify-influencers/src/App.jsx
+++ verify-influencers/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { ArrowLeft } from "lucide-react"
 import React from "react"
 import MenuBar from "./components/MenuBar";
 import Main from "./components/BodyComponent"
@@ -9,6 +10,12 @@ function App() {
 
   return (
     <>
+    <div className="bg-[#101724]">
+          <button className="absolute top-4 left flex items-center gap-2 bg-transparent text-white border border-white rounded-lg py-2 px-4 hover:bg-opacity-50 focus:outline-none">
+            <ArrowLeft className="w-5 h-5" />
+            <span>back to dashboard</span>
+          </button>
+        </div>
       <div>
         <MenuBar />
         <Main />
