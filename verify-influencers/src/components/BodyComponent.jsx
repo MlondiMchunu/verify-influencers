52211d7605f1a0e17ceee3967d647741ccb3ae3b
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-01-29 18:23:48 +0200

    create research configuration header

--- verify-influencers/src/components/BodyComponent.jsx
+++ verify-influencers/src/components/BodyComponent.jsx
@@ -6,7 +6,15 @@ import {Settings} from "lucide-react";
 export default function BodyComponent(){
     return (
         <>
-        <div className="bg-[#182130] w-[calc(100vw-100px)] h-[calc(100vh-100px)] mx-auto my-[50px] rounded-2xl flex items-center justify-center"></div>
+        <div className="bg-[#182130] w-[calc(100vw-100px)] h-[calc(100vh-100px)] mx-auto my-[50px] rounded-2xl flex items-center justify-center">
+            <div className="flex items-center gap-3">
+                <Settings className="w-6 h-6 text-[#1db687]"/>
+                <h2 className="text-xl fint-semibold">Research Configuration</h2>
+            </div>
+            <div className="mt-6">
+                <p>WElcome to Research Configuration Panel.</p>
+            </div>
+        </div>
         </>
-    )
+    );
 }
