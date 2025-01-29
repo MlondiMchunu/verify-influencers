6d7ca7c75a790546439e5dde3f0f0d89333d5835
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-01-29 16:10:39 +0200

    latest commit

--- verify-influencers/src/components/BackToDashboard.jsx
+++ verify-influencers/src/components/BackToDashboard.jsx
@@ -4,9 +4,9 @@ import {ArrowLeft} from "lucide-react";
 export default function BackToDashboard(){
     return(
         <button
-            className="fixed top-[60px] bg-transparent hover:bg-gray-800 text-white p-2 rounded-full shadow-md opacity-0 hover:opacity-100 transition-opacity duration-300"
+            className="fixed top-[60px] left-[50px] bg-transparent hover:bg-gray-800 text-white p-2 rounded-full shadow-md opacity-0 hover:opacity-100 transition-opacity duration-300"
             onClick={()=> window.history.back()}>
                 <ArrowLeft className="w-6 h-6"/>
             </button>
-    )
+    );
 }
