384f875f8795f6a9cd9ac7f573ddb5c000681098
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-01 23:45:14 +0200

    latest commit

--- verify-influencers/src/components/InfluencerTrust.jsx
+++ verify-influencers/src/components/InfluencerTrust.jsx
@@ -1,50 +1,50 @@
 import React from "react";
 
 const InfluencerTrust = () => {
-    return (
+  return (
 
-        <>
-            <div className="w-full text-white text-left  max-w-[1200px] mx-auto bg-[#182130] p-6 rounded-lg shadow-md">
-                {/* Header */}
-                <h2 className=" text-xl font-bold">Influencer Trust Leaderboard</h2>
-                <p className="text-xs/5 mt-2 opacity-80">Real-time rankings of health influencers based on scientific accuracy, credibility, and transparency. Updated daily using AI-poered analysis.</p>
+    <>
+      <div className="w-full text-white text-left  max-w-[1200px] mx-auto bg-[#101727] p-6 rounded-lg shadow-md">
+        {/* Header */}
+        <h2 className=" text-xl font-bold">Influencer Trust Leaderboard</h2>
+        <p className="text-xs/5 mt-2 opacity-80">Real-time rankings of health influencers based on scientific accuracy, credibility, and transparency. Updated daily using AI-powered analysis.</p>
 
 
 
-                <div className="flex justify-between mt-4 text-white">
-                    <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
-                        <p className="text-xs text-gray-400">Active Influencers</p>
-                        <p className="text-lg font-semibold">--</p>
-                    </label>
-                    <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
-                        <p className="text-xs text-gray-400">Claims Verified</p>
-                        <p className="text-lg font-semibold">--</p>
-                    </label>
-                    <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
-                        <p className="text-xs text-gray-400">Average Trust Score</p>
-                        <p className="text-lg font-semibold">--</p>
-                    </label>
-                </div>
-                <div className="flex justify-start space-x-4 mt-6">
-        <button className=" text-white border border-gray-500 px-4 py-2 rounded-xl hover:bg-gray-700 transition">
-          All
-        </button>
-        <button className="bg-[#1f2937] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
-          Nutrition
-        </button>
-        <button className="bg-[#1f2937] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
-          Fitness
-        </button>
-        <button className="bg-[#1f2937] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
-          Medicine
-        </button>
-        <button className="bg-[#1f2937] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
-          Mental Health
-        </button>
+        <div className="flex justify-between mt-4 text-white ">
+          <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
+            <p className="text-xs text-gray-400">Active Influencers</p>
+            <p className="text-lg font-semibold">--</p>
+          </label>
+          <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
+            <p className="text-xs text-gray-400">Claims Verified</p>
+            <p className="text-lg font-semibold">--</p>
+          </label>
+          <label className="bg-[#1f2937] px-6 py-3 rounded-sm shadow border border-white-50 border-opacity-50">
+            <p className="text-xs text-gray-400">Average Trust Score</p>
+            <p className="text-lg font-semibold">--</p>
+          </label>
+        </div>
+        <div className="w-1/2 mt-6 flex gap-2 text-xs/5">
+          <button className="bg-[#182130] !bg-[#182130] text-white border border-white-50 px-4 py-2 rounded-md">
+            All
+          </button>
+          <button className="bg-[#182130] !bg-[#182130] text-white border border-gray-600 px-4 py-2 rounded-md text-xs">
+            Nutrition
+          </button>
+          <button className="bg-[#182130] !bg-[#182130] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
+            Fitness
+          </button>
+          <button className="bg-[#182130] !bg-[#182130] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
+            Medicine
+          </button>
+          <button className="bg-[#182130] !bg-[#182130] text-white border border-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
+            Mental Health
+          </button>
+        </div>
       </div>
-            </div>
-        </>
-    );
+    </>
+  );
 };
 
 export default InfluencerTrust;
