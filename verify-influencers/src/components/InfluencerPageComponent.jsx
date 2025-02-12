ee3cf0ff6318a8cd339346fcf8fd7b95f8c70905
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-13 00:23:52 +0200

    commit influencer page component

--- verify-influencers/src/components/InfluencerPageComponent.jsx
+++ verify-influencers/src/components/InfluencerPageComponent.jsx
@@ -1,6 +1,8 @@
-export default function InfluencerPageComponent(){
-    return(
+export default function InfluencerPageComponent() {
+    return (
         <>
+            <div className="w-full text-white text-left  max-w-[1200px] mx-auto bg-[#101727] p-6 rounded-lg shadow-md">
+            </div>
         </>
     )
 }
