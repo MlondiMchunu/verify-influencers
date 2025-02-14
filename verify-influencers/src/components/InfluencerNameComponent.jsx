34539574a5eb45d5fc0f6d9682d68561d05be25a
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-14 17:04:33 +0200

    update influence name component with relevant props

--- verify-influencers/src/components/InfluencerNameComponent.jsx
+++ verify-influencers/src/components/InfluencerNameComponent.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Search } from "lucide-react"; // import the Search icon
 
-export default function InfluencerNameComponent() {
+export default function InfluencerNameComponent(value, onChange) {
   return (
     <div className="flex items-center justify-center w-full max-w-md mx-auto mt-1">
       <div className="relative w-full">
         {/* Search input container */}
         <input
           type="text"
+          value={value}
           placeholder="Enter influencer name"
           className="w-full py-2 pl-10 pr-4 rounded-sm bg-[#101727] text-white text-xs/5 border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
-        
+
         {/* Search Icon */}
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
       </div>
