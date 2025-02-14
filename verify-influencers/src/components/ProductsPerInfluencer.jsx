9f636e535e03af9c01873dd3fd593394ac3afdc6
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-14 17:10:01 +0200

    onChange props

--- verify-influencers/src/components/ProductsPerInfluencer.jsx
+++ verify-influencers/src/components/ProductsPerInfluencer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Search } from "lucide-react"; // import the Search icon
 
-export default function ProductsPerInfluencer() {
+export default function ProductsPerInfluencer(value, onChange) {
   return (
     <div className="flex items-center justify-start w-full max-w-md mx-auto mt-1">
       <div className="relative w-full">
         {/* Search input container */}
         <input
-          type="text"
+          type="number"
+          value={value}
           className="ml-2 mt-0 flex w-full py-2 pl-10 pr-4 rounded-sm bg-[#101727] text-white text-xs/5 border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent h-7"
         />
 
