c970062f92de971be3870434f24f44467c9b642d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-14 17:11:48 +0200

    onChange props on productsperinfluencer component

--- verify-influencers/src/components/ProductsPerInfluencer.jsx
+++ verify-influencers/src/components/ProductsPerInfluencer.jsx
@@ -9,6 +9,7 @@ export default function ProductsPerInfluencer(value, onChange) {
         <input
           type="number"
           value={value}
+          onChange={onChange}
           className="ml-2 mt-0 flex w-full py-2 pl-10 pr-4 rounded-sm bg-[#101727] text-white text-xs/5 border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent h-7"
         />
 
