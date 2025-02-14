6608ab1c1988092ea0a63ec8068aaa60720a6810
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-02-14 17:06:56 +0200

    update props in claimsPerInfluencer component

--- verify-influencers/src/components/ClaimsPerInfluencer.jsx
+++ verify-influencers/src/components/ClaimsPerInfluencer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Search } from "lucide-react"; // import the Search icon
 
-export default function ClaimsPerInfluencer() {
+export default function ClaimsPerInfluencer(value,onChange) {
   return (
     <div className="flex items-center justify-center w-full max-w-md mx-auto mt-1">
       <div className="relative w-full">
         {/* Search input container */}
         <input
-          type="text"
+          type="number"
+          value={value}
+          placeholder="Enter number of claims"
           className="flex w-full py-2 pr-4 rounded-sm bg-[#101727] text-white text-xs/5 border border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
 
